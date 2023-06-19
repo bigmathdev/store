6f77dccf616342eebd24bd30209bd20e086d5139
Matheus <bigmath00@gmail.com>
2023-06-19 11:05:58 -0300

    chore (Produtos) : Implementando API fake store

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,9 @@
           <span class="name">{{ product.title }}</span>
           <span class="price">R${{ product.price }}</span>
           <div class="quantity-area" v-if="product.active">
-            <button @click.stop="quantity--" :disabled="quantity <= 1">-</button>
-            <span class="quantity">{{ quantity }}</span>
-            <button @click.stop="quantity++">+</button>
+            <button @click.stop="product.quantity--" :disabled="product.quantity <= 1">-</button>
+            <span class="quantity">{{ product.quantity }}</span>
+            <button @click.stop="product.quantity++">+</button>
           </div>
         </div>
       </div>
@@ -29,9 +29,9 @@
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1x </td>
-            <td>5.99</td>
+          <tr v-for="product in carrinho">
+            <td>{{ product.quantidade + 'x ' + product.nome  }}</td>
+            <td>{{ product.quantidade * product.preco }}</td>
           </tr>
 
           <tr>
@@ -46,7 +46,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed } from 'vue'
 import axios from "axios";
 
 const products = ref(undefined)
@@ -55,9 +55,23 @@ const quantity = ref(1)
 const storeAPI = async () => {
   const response = await axios.get('https://fakestoreapi.com/products')
   products.value = response.data
+  products.value.forEach(i => {
+    i.quantity = 0
+  })
   console.log(response)
   return response
 }
+
+const carrinho = computed(() => {
+  return products.value?.filter(f => f.quantity > 0).map(m => {
+    return {
+      id: m.id,
+      nome: m.title,
+      preco: m.price,
+      quantidade: m.quantity
+    }
+  })
+})
 
 onMounted(() => {
   storeAPI()
